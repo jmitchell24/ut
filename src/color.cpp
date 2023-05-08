f047c3ab2df48193ee1b928ec83f55ba69996867
james <mail@jpmitchell.ca>
2023-05-08 09:49:54 -0400

    forgot to add cpp files

---
+++ src/color.cpp
@@ -0,0 +1,55 @@
+//
+// Created by james on 29/03/22.
+//
+
+#include "ut/color.hpp"
+
+#include <cmath>
+using namespace std;
+
+using namespace ut;
+
+inline bool try_parse(char const* s, uint8_t& x1, uint8_t& x2, uint8_t& x3, uint8_t& x4)
+{
+    if (s == nullptr)
+        return false;
+
+    char const* fmt = s[0] == '#' ? "#%02X%02X%02X%02X" : "%02X%02X%02X%02X";
+
+    unsigned t[4];
+    if (auto res = sscanf(s, fmt, t+0, t+1, t+2, t+3); res == 4)
+    {
+        x1 = t[0]; x2 = t[1]; x3 = t[2]; x4 = t[3];
+        return true;
+    }
+    return false;
+}
+
+inline bool try_parse(char const* s, uint8_t& x1, uint8_t& x2, uint8_t& x3)
+{
+    if (s == nullptr)
+        return false;
+
+    char const* fmt = s[0] == '#' ? "#%02X%02X%02X" : "%02X%02X%02X";
+
+    unsigned t[3];
+    if (auto res = sscanf(s, fmt, t+0, t+1, t+2); res == 4)
+    {
+        x1 = t[0]; x2 = t[1]; x3 = t[2];
+        return true;
+    }
+    return false;
+}
+
+//
+// color
+//
+
+color color::parseRGBA(char const* s) { color c; try_parse(s, c.r, c.g, c.b, c.a); return c; }
+bool color::tryParseRGBA(char const* s, color& c) { return try_parse(s, c.r, c.g, c.b, c.a); }
+
+color color::parseARGB(char const* s) { color c; try_parse(s, c.a, c.r, c.g, c.b); return c; }
+bool color::tryParseARGB(char const* s, color& c) { return try_parse(s, c.a, c.r, c.g, c.b); }
+
+
+
---
+++ src/math.cpp
@@ -0,0 +1,39 @@
+#include <ut/math.hpp>
+
+namespace ut
+{
+    template struct vec<float       , 2>;
+    template struct vec<double      , 2>;
+    template struct vec<int         , 2>;
+    template struct vec<unsigned    , 2>;
+    template struct vec<std::uint8_t, 2>;
+
+    template struct vec<float       , 3>;
+    template struct vec<double      , 3>;
+    template struct vec<int         , 3>;
+    template struct vec<unsigned    , 3>;
+    template struct vec<std::uint8_t, 3>;
+
+    template struct vec<float       , 4>;
+    template struct vec<double      , 4>;
+    template struct vec<int         , 4>;
+    template struct vec<unsigned    , 4>;
+    template struct vec<std::uint8_t, 4>;
+
+    template struct rectx<float       , false>;
+    template struct rectx<double      , false>;
+    template struct rectx<int         , false>;
+    template struct rectx<unsigned    , false>;
+    template struct rectx<std::uint8_t, false>;
+
+    template struct rectx<int         , true>;
+    template struct rectx<unsigned    , true>;
+    template struct rectx<std::uint8_t, true>;
+
+    template struct psizex<float       >;
+    template struct psizex<double      >;
+    template struct psizex<int         >;
+    template struct psizex<unsigned    >;
+    //template struct psizex<std::uint8_t>;
+
+}
