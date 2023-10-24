c10c7ad93b9f74f5c670999b7895cc5c4dfd9ebe
james <mail@jpmitchell.ca>
2023-10-24 08:52:00 -0400

    more matrix updates

--- include/ut/math/matrix4D.hpp
+++ include/ut/math/matrix4D.hpp
@@ -51,6 +51,8 @@ namespace ut
 {
     template <typename N> struct mat<N,4,4>
     {
+        struct transpose_ctor_tag {};
+
         using scalar_type       = N;
         using vector2_type      = vec2x<N>;
         using vector3_type      = vec3x<N>;
@@ -73,10 +75,10 @@ namespace ut
             struct
             {
                 scalar_type
-                    m0, m4, m8 , m12,
-                    m1, m5, m9 , m13,
-                    m2, m6, m10, m14,
-                    m3, m7, m11, m15;
+                    m11, m21, m31, m41,
+                    m12, m22, m32, m42,
+                    m13, m23, m33, m43,
+                    m14, m24, m34, m44;
             };
 
             pack_type pack;
@@ -86,25 +88,38 @@ namespace ut
         // ctor
         //
 
-        M_DECL mat()
-        { EXPAND_M(pack, _, M_SET_ZERO) }
-
-        M_DECL explicit mat(scalar_param s)
-        { EXPAND_M(pack, s, M_SET_SCALAR) }
-
-        M_DECL explicit mat(pack_type const& p)
-        { EXPAND_M(pack, p, M_SET_ARRAY) }
+        M_DECL mat() : mat{
+            one_ , zero_, zero_, zero_,
+            zero_, one_ , zero_, zero_,
+            zero_, zero_, one_ , zero_,
+            zero_, zero_, zero_, one_
+        } {}
 
         M_DECL explicit mat(
-            scalar_type m0, scalar_type m4, scalar_type m8 , scalar_type m12,
-            scalar_type m1, scalar_type m5, scalar_type m9 , scalar_type m13,
-            scalar_type m2, scalar_type m6, scalar_type m10, scalar_type m14,
-            scalar_type m3, scalar_type m7, scalar_type m11, scalar_type m15) :
-            m0{m0}, m4{m4}, m8 {m8 }, m12{m12},
-            m1{m1}, m5{m5}, m9 {m9 }, m13{m13},
-            m2{m2}, m6{m6}, m10{m10}, m14{m14},
-            m3{m3}, m7{m7}, m11{m11}, m15{m15}
+                     scalar_type m11, scalar_type m12, scalar_type m13, scalar_type m14,
+                     scalar_type m21, scalar_type m22, scalar_type m23, scalar_type m24,
+                     scalar_type m31, scalar_type m32, scalar_type m33, scalar_type m34,
+                     scalar_type m41, scalar_type m42, scalar_type m43, scalar_type m44) :
+            m11{m11}, m21{m21}, m31{m31}, m41{m41},
+            m12{m12}, m22{m22}, m32{m32}, m42{m42},
+            m13{m13}, m23{m23}, m33{m33}, m43{m43},
+            m14{m14}, m24{m24}, m34{m34}, m44{m44}
         {}
+
+        M_DECL explicit mat(
+            scalar_type m11, scalar_type m12, scalar_type m13, scalar_type m14,
+            scalar_type m21, scalar_type m22, scalar_type m23, scalar_type m24,
+            scalar_type m31, scalar_type m32, scalar_type m33, scalar_type m34,
+            scalar_type m41, scalar_type m42, scalar_type m43, scalar_type m44,
+            transpose_ctor_tag) :
+            m11{m11}, m21{m12}, m31{m13}, m41{m14},
+            m12{m21}, m22{m22}, m32{m23}, m42{m24},
+            m13{m31}, m23{m32}, m33{m33}, m43{m34},
+            m14{m41}, m24{m42}, m34{m43}, m44{m44}
+        {}
+
+        M_DECL explicit mat(scalar_param s) { EXPAND_M(pack, s, M_SET_SCALAR) }
+        M_DECL explicit mat(pack_type const& p) { EXPAND_M(pack, p, M_SET_ARRAY) }
 
         M_DECL mat(matrix_type const&)=default;
         M_DECL mat(matrix_type&&) noexcept =default;
@@ -170,18 +185,6 @@ namespace ut
         // mutators
         //
 
-        M_DECL void set(
-            scalar_type m0, scalar_type m4, scalar_type m8 , scalar_type m12,
-            scalar_type m1, scalar_type m5, scalar_type m9 , scalar_type m13,
-            scalar_type m2, scalar_type m6, scalar_type m10, scalar_type m14,
-            scalar_type m3, scalar_type m7, scalar_type m11, scalar_type m15)
-        {
-            this->m0=m0; this->m4=m4; this->m8 =m8 ; this->m12=m12;
-            this->m1=m1; this->m5=m5; this->m9 =m9 ; this->m13=m13;
-            this->m2=m2; this->m6=m6; this->m10=m10; this->m14=m14;
-            this->m3=m3; this->m7=m7; this->m11=m11; this->m15=m15;
-        }
-
         M_DECL void set(scalar_param s)     { EXPAND_M(pack, s     , M_SET_SCALAR) }
         M_DECL void set(matrix_param m)     { EXPAND_M(pack, m.pack, M_SET_ARRAY ) }
         M_DECL void set(pack_type const& p) { EXPAND_M(pack, p     , M_SET_ARRAY ) }
@@ -192,7 +195,7 @@ namespace ut
         M_DECL void mul(matrix_param m)
         {
             matrix_type tmp{noinit_tag{}};
-            mul(tmp, *this, m);
+            mul(tmp.pack, pack, m.pack);
             set(tmp);
         }
 
@@ -200,10 +203,10 @@ namespace ut
         {
             matrix_type tmp{noinit_tag{}};
 
-            tmp.m0  = m0; tmp.m1  = m4; tmp.m2  =  m8; tmp.m3  = m12;
-            tmp.m4  = m1; tmp.m5  = m5; tmp.m6  =  m9; tmp.m7  = m13;
-            tmp.m8  = m2; tmp.m9  = m6; tmp.m10 = m10; tmp.m11 = m14;
-            tmp.m12 = m3; tmp.m13 = m7; tmp.m14 = m11; tmp.m15 = m15;
+            tmp.m11=m11; tmp.m21=m12; tmp.m31=m13; tmp.m41=m14;
+            tmp.m12=m21; tmp.m22=m22; tmp.m32=m23; tmp.m42=m24;
+            tmp.m13=m31; tmp.m23=m32; tmp.m33=m33; tmp.m43=m34;
+            tmp.m14=m41; tmp.m24=m42; tmp.m34=m43; tmp.m44=m44;
 
             set(tmp);
         }
@@ -211,6 +214,11 @@ namespace ut
         //
         // utilities
         //
+
+        M_DECL_PURE scalar_type trace() const
+        {
+            return m11 + m22 + m33 + m44;
+        }
 
         M_DECL_PURE matrix_type round() const
         {
@@ -247,8 +255,8 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4,
-                p.x * m1 + p.y * m5
+                p.x * pack[0] + p.y * pack[4],
+                p.x * pack[1] + p.y * pack[5]
             };
         }
 
@@ -256,9 +264,9 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4 + p.z * m8 ,
-                p.x * m1 + p.y * m5 + p.z * m9 ,
-                p.x * m2 + p.y * m6 + p.z * m10
+                p.x * pack[0] + p.y * pack[4] + p.z * pack[8 ],
+                p.x * pack[1] + p.y * pack[5] + p.z * pack[9 ],
+                p.x * pack[2] + p.y * pack[6] + p.z * pack[10]
             };
         }
 
@@ -266,10 +274,10 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4 + p.z * m8  + p.w * m12,
-                p.x * m1 + p.y * m5 + p.z * m9  + p.w * m13,
-                p.x * m2 + p.y * m6 + p.z * m10 + p.w * m14,
-                p.x * m3 + p.y * m7 + p.z * m11 + p.w * m15
+                p.x * pack[0] + p.y * pack[4] + p.z * pack[8 ] + p.w * pack[12],
+                p.x * pack[1] + p.y * pack[5] + p.z * pack[9 ] + p.w * pack[13],
+                p.x * pack[2] + p.y * pack[6] + p.z * pack[10] + p.w * pack[14],
+                p.x * pack[3] + p.y * pack[7] + p.z * pack[11] + p.w * pack[15]
             };
         }
 
@@ -277,8 +285,8 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4 + m12,
-                p.x * m1 + p.y * m5 + m13
+                p.x * pack[0] + p.y * pack[4] + pack[12],
+                p.x * pack[1] + p.y * pack[5] + pack[13]
             };
         }
 
@@ -286,9 +294,9 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4 + p.z * m8  + m12,
-                p.x * m1 + p.y * m5 + p.z * m9  + m13,
-                p.x * m2 + p.y * m6 + p.z * m10 + m14
+                p.x * pack[0] + p.y * pack[4] + p.z * pack[8 ] + pack[12],
+                p.x * pack[1] + p.y * pack[5] + p.z * pack[9 ] + pack[13],
+                p.x * pack[2] + p.y * pack[6] + p.z * pack[10] + pack[14]
             };
         }
 
@@ -296,10 +304,10 @@ namespace ut
         {
             return
             {
-                p.x * m0 + p.y * m4 + p.z * m8  + m12,
-                p.x * m1 + p.y * m5 + p.z * m9  + m13,
-                p.x * m2 + p.y * m6 + p.z * m10 + m14,
-                p.x * m3 + p.y * m7 + p.z * m11 + m15
+                p.x * pack[0] + p.y * pack[4] + p.z * pack[8 ] + pack[12],
+                p.x * pack[1] + p.y * pack[5] + p.z * pack[9 ] + pack[13],
+                p.x * pack[2] + p.y * pack[6] + p.z * pack[10] + pack[14],
+                p.x * pack[3] + p.y * pack[7] + p.z * pack[11] + pack[15]
             };
         }
 
@@ -322,33 +330,33 @@ namespace ut
         // static utilities
         //
 
-        M_DECL static void mul(matrix_type& tmp, matrix_param a, matrix_param b)
+        M_DECL static void mul(pack_type& tmp, pack_type const& a, pack_type const& b)
         {
-            tmp.m0  = a.m0  * b.m0 + a.m1  * b.m4 + a.m2  * b.m8  + a.m3  * b.m12;
-            tmp.m1  = a.m0  * b.m1 + a.m1  * b.m5 + a.m2  * b.m9  + a.m3  * b.m13;
-            tmp.m2  = a.m0  * b.m2 + a.m1  * b.m6 + a.m2  * b.m10 + a.m3  * b.m14;
-            tmp.m3  = a.m0  * b.m3 + a.m1  * b.m7 + a.m2  * b.m11 + a.m3  * b.m15;
-            tmp.m4  = a.m4  * b.m0 + a.m5  * b.m4 + a.m6  * b.m8  + a.m7  * b.m12;
-            tmp.m5  = a.m4  * b.m1 + a.m5  * b.m5 + a.m6  * b.m9  + a.m7  * b.m13;
-            tmp.m6  = a.m4  * b.m2 + a.m5  * b.m6 + a.m6  * b.m10 + a.m7  * b.m14;
-            tmp.m7  = a.m4  * b.m3 + a.m5  * b.m7 + a.m6  * b.m11 + a.m7  * b.m15;
-            tmp.m8  = a.m8  * b.m0 + a.m9  * b.m4 + a.m10 * b.m8  + a.m11 * b.m12;
-            tmp.m9  = a.m8  * b.m1 + a.m9  * b.m5 + a.m10 * b.m9  + a.m11 * b.m13;
-            tmp.m10 = a.m8  * b.m2 + a.m9  * b.m6 + a.m10 * b.m10 + a.m11 * b.m14;
-            tmp.m11 = a.m8  * b.m3 + a.m9  * b.m7 + a.m10 * b.m11 + a.m11 * b.m15;
-            tmp.m12 = a.m12 * b.m0 + a.m13 * b.m4 + a.m14 * b.m8  + a.m15 * b.m12;
-            tmp.m13 = a.m12 * b.m1 + a.m13 * b.m5 + a.m14 * b.m9  + a.m15 * b.m13;
-            tmp.m14 = a.m12 * b.m2 + a.m13 * b.m6 + a.m14 * b.m10 + a.m15 * b.m14;
-            tmp.m15 = a.m12 * b.m3 + a.m13 * b.m7 + a.m14 * b.m11 + a.m15 * b.m15;
+            tmp[0 ] = a[0 ] * b[0] + a[1 ] * b[4] + a[2 ] * b[8 ] + a[3 ] * b[12];
+            tmp[1 ] = a[0 ] * b[1] + a[1 ] * b[5] + a[2 ] * b[9 ] + a[3 ] * b[13];
+            tmp[2 ] = a[0 ] * b[2] + a[1 ] * b[6] + a[2 ] * b[10] + a[3 ] * b[14];
+            tmp[3 ] = a[0 ] * b[3] + a[1 ] * b[7] + a[2 ] * b[11] + a[3 ] * b[15];
+            tmp[4 ] = a[4 ] * b[0] + a[5 ] * b[4] + a[6 ] * b[8 ] + a[7 ] * b[12];
+            tmp[5 ] = a[4 ] * b[1] + a[5 ] * b[5] + a[6 ] * b[9 ] + a[7 ] * b[13];
+            tmp[6 ] = a[4 ] * b[2] + a[5 ] * b[6] + a[6 ] * b[10] + a[7 ] * b[14];
+            tmp[7 ] = a[4 ] * b[3] + a[5 ] * b[7] + a[6 ] * b[11] + a[7 ] * b[15];
+            tmp[8 ] = a[8 ] * b[0] + a[9 ] * b[4] + a[10] * b[8 ] + a[11] * b[12];
+            tmp[9 ] = a[8 ] * b[1] + a[9 ] * b[5] + a[10] * b[9 ] + a[11] * b[13];
+            tmp[10] = a[8 ] * b[2] + a[9 ] * b[6] + a[10] * b[10] + a[11] * b[14];
+            tmp[11] = a[8 ] * b[3] + a[9 ] * b[7] + a[10] * b[11] + a[11] * b[15];
+            tmp[12] = a[12] * b[0] + a[13] * b[4] + a[14] * b[8 ] + a[15] * b[12];
+            tmp[13] = a[12] * b[1] + a[13] * b[5] + a[14] * b[9 ] + a[15] * b[13];
+            tmp[14] = a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14];
+            tmp[15] = a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15];
         }
 
-        M_DECL static void inv(matrix_type& tmp, matrix_param mat)
+        M_DECL static void inv(pack_type& tmp, pack_type const& mat)
         {
             // Cache the matrix values (speed optimization)
-            float a00 = mat.m0 , a01 = mat.m1 , a02 = mat.m2 , a03 = mat.m3;
-            float a10 = mat.m4 , a11 = mat.m5 , a12 = mat.m6 , a13 = mat.m7;
-            float a20 = mat.m8 , a21 = mat.m9 , a22 = mat.m10, a23 = mat.m11;
-            float a30 = mat.m12, a31 = mat.m13, a32 = mat.m14, a33 = mat.m15;
+            float a00 = mat[0 ], a01 = mat[1 ], a02 = mat[2 ], a03 = mat[3 ];
+            float a10 = mat[4 ], a11 = mat[5 ], a12 = mat[6 ], a13 = mat[7 ];
+            float a20 = mat[8 ], a21 = mat[9 ], a22 = mat[10], a23 = mat[11];
+            float a30 = mat[12], a31 = mat[13], a32 = mat[14], a33 = mat[15];
 
             float b00 = a00*a11 - a01*a10;
             float b01 = a00*a12 - a02*a10;
@@ -366,22 +374,22 @@ namespace ut
             // Calculate the invert determinant (inlined to avoid double-caching)
             float invDet = one_ / (b00*b11 - b01*b10 + b02*b09 + b03*b08 - b04*b07 + b05*b06);
 
-            tmp.m0  = ( a11*b11 - a12*b10 + a13*b09)*invDet;
-            tmp.m1  = (-a01*b11 + a02*b10 - a03*b09)*invDet;
-            tmp.m2  = ( a31*b05 - a32*b04 + a33*b03)*invDet;
-            tmp.m3  = (-a21*b05 + a22*b04 - a23*b03)*invDet;
-            tmp.m4  = (-a10*b11 + a12*b08 - a13*b07)*invDet;
-            tmp.m5  = ( a00*b11 - a02*b08 + a03*b07)*invDet;
-            tmp.m6  = (-a30*b05 + a32*b02 - a33*b01)*invDet;
-            tmp.m7  = ( a20*b05 - a22*b02 + a23*b01)*invDet;
-            tmp.m8  = ( a10*b10 - a11*b08 + a13*b06)*invDet;
-            tmp.m9  = (-a00*b10 + a01*b08 - a03*b06)*invDet;
-            tmp.m10 = ( a30*b04 - a31*b02 + a33*b00)*invDet;
-            tmp.m11 = (-a20*b04 + a21*b02 - a23*b00)*invDet;
-            tmp.m12 = (-a10*b09 + a11*b07 - a12*b06)*invDet;
-            tmp.m13 = ( a00*b09 - a01*b07 + a02*b06)*invDet;
-            tmp.m14 = (-a30*b03 + a31*b01 - a32*b00)*invDet;
-            tmp.m15 = ( a20*b03 - a21*b01 + a22*b00)*invDet;
+            tmp[0 ] = ( a11*b11 - a12*b10 + a13*b09)*invDet;
+            tmp[1 ] = (-a01*b11 + a02*b10 - a03*b09)*invDet;
+            tmp[2 ] = ( a31*b05 - a32*b04 + a33*b03)*invDet;
+            tmp[3 ] = (-a21*b05 + a22*b04 - a23*b03)*invDet;
+            tmp[4 ] = (-a10*b11 + a12*b08 - a13*b07)*invDet;
+            tmp[5 ] = ( a00*b11 - a02*b08 + a03*b07)*invDet;
+            tmp[6 ] = (-a30*b05 + a32*b02 - a33*b01)*invDet;
+            tmp[7 ] = ( a20*b05 - a22*b02 + a23*b01)*invDet;
+            tmp[8 ] = ( a10*b10 - a11*b08 + a13*b06)*invDet;
+            tmp[9 ] = (-a00*b10 + a01*b08 - a03*b06)*invDet;
+            tmp[10] = ( a30*b04 - a31*b02 + a33*b00)*invDet;
+            tmp[11] = (-a20*b04 + a21*b02 - a23*b00)*invDet;
+            tmp[12] = (-a10*b09 + a11*b07 - a12*b06)*invDet;
+            tmp[13] = ( a00*b09 - a01*b07 + a02*b06)*invDet;
+            tmp[14] = (-a30*b03 + a31*b01 - a32*b00)*invDet;
+            tmp[15] = ( a20*b03 - a21*b01 + a22*b00)*invDet;
         }
 
         //
@@ -416,22 +424,22 @@ namespace ut
     template <typename N> constexpr mat<N,4,4>::mat(std::nullptr_t)
     {
         using mat_t = mat<N,4,4>;
-        static_assert(offsetof(mat_t, m0 ) == offsetof(mat_t, pack) + sizeof(N)*0 , "wrong 'm0'  layout in pack");
-        static_assert(offsetof(mat_t, m4 ) == offsetof(mat_t, pack) + sizeof(N)*1 , "wrong 'm4'  layout in pack");
-        static_assert(offsetof(mat_t, m8 ) == offsetof(mat_t, pack) + sizeof(N)*2 , "wrong 'm8'  layout in pack");
-        static_assert(offsetof(mat_t, m12) == offsetof(mat_t, pack) + sizeof(N)*3 , "wrong 'm12' layout in pack");
-        static_assert(offsetof(mat_t, m1 ) == offsetof(mat_t, pack) + sizeof(N)*4 , "wrong 'm1'  layout in pack");
-        static_assert(offsetof(mat_t, m5 ) == offsetof(mat_t, pack) + sizeof(N)*5 , "wrong 'm5'  layout in pack");
-        static_assert(offsetof(mat_t, m9 ) == offsetof(mat_t, pack) + sizeof(N)*6 , "wrong 'm9'  layout in pack");
-        static_assert(offsetof(mat_t, m13) == offsetof(mat_t, pack) + sizeof(N)*7 , "wrong 'm13' layout in pack");
-        static_assert(offsetof(mat_t, m2 ) == offsetof(mat_t, pack) + sizeof(N)*8 , "wrong 'm2'  layout in pack");
-        static_assert(offsetof(mat_t, m6 ) == offsetof(mat_t, pack) + sizeof(N)*9 , "wrong 'm6'  layout in pack");
-        static_assert(offsetof(mat_t, m10) == offsetof(mat_t, pack) + sizeof(N)*10, "wrong 'm10' layout in pack");
-        static_assert(offsetof(mat_t, m14) == offsetof(mat_t, pack) + sizeof(N)*11, "wrong 'm14' layout in pack");
-        static_assert(offsetof(mat_t, m3 ) == offsetof(mat_t, pack) + sizeof(N)*12, "wrong 'm3'  layout in pack");
-        static_assert(offsetof(mat_t, m7 ) == offsetof(mat_t, pack) + sizeof(N)*13, "wrong 'm7'  layout in pack");
-        static_assert(offsetof(mat_t, m11) == offsetof(mat_t, pack) + sizeof(N)*14, "wrong 'm11' layout in pack");
-        static_assert(offsetof(mat_t, m15) == offsetof(mat_t, pack) + sizeof(N)*15, "wrong 'm15' layout in pack");
+        static_assert(offsetof(mat_t, m11) == offsetof(mat_t, pack) + sizeof(N)*0 , "wrong 'm11' layout in pack");
+        static_assert(offsetof(mat_t, m21) == offsetof(mat_t, pack) + sizeof(N)*1 , "wrong 'm21' layout in pack");
+        static_assert(offsetof(mat_t, m31) == offsetof(mat_t, pack) + sizeof(N)*2 , "wrong 'm31' layout in pack");
+        static_assert(offsetof(mat_t, m41) == offsetof(mat_t, pack) + sizeof(N)*3 , "wrong 'm41' layout in pack");
+        static_assert(offsetof(mat_t, m12) == offsetof(mat_t, pack) + sizeof(N)*4 , "wrong 'm12' layout in pack");
+        static_assert(offsetof(mat_t, m22) == offsetof(mat_t, pack) + sizeof(N)*5 , "wrong 'm22' layout in pack");
+        static_assert(offsetof(mat_t, m32) == offsetof(mat_t, pack) + sizeof(N)*6 , "wrong 'm32' layout in pack");
+        static_assert(offsetof(mat_t, m42) == offsetof(mat_t, pack) + sizeof(N)*7 , "wrong 'm42' layout in pack");
+        static_assert(offsetof(mat_t, m13) == offsetof(mat_t, pack) + sizeof(N)*8 , "wrong 'm13' layout in pack");
+        static_assert(offsetof(mat_t, m23) == offsetof(mat_t, pack) + sizeof(N)*9 , "wrong 'm23' layout in pack");
+        static_assert(offsetof(mat_t, m33) == offsetof(mat_t, pack) + sizeof(N)*10, "wrong 'm33' layout in pack");
+        static_assert(offsetof(mat_t, m43) == offsetof(mat_t, pack) + sizeof(N)*11, "wrong 'm43' layout in pack");
+        static_assert(offsetof(mat_t, m14) == offsetof(mat_t, pack) + sizeof(N)*12, "wrong 'm14' layout in pack");
+        static_assert(offsetof(mat_t, m24) == offsetof(mat_t, pack) + sizeof(N)*13, "wrong 'm24' layout in pack");
+        static_assert(offsetof(mat_t, m34) == offsetof(mat_t, pack) + sizeof(N)*14, "wrong 'm34' layout in pack");
+        static_assert(offsetof(mat_t, m44) == offsetof(mat_t, pack) + sizeof(N)*15, "wrong 'm44' layout in pack");
     }
 
     template <typename N> using mat4x = mat<N,4,4>;
