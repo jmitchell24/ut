fde581cbb718ec58a4de10dbb0eee62a5543503b
James Mitchell <mail@jpmitchell.ca>
2025-04-20 10:42:23 -0400

    Changes: - fixed term.hpp file name.

---
+++ include/ut/term.hpp
@@ -0,0 +1,22 @@
+//
+// Created by james on 02/04/25.
+//
+
+#pragma once
+
+#include "ut/term/escapes.hpp"
+#include "ut/term/rawterm.hpp"
+#include "ut/term/shell.hpp"
+#include "ut/term/tui.hpp"
+
+
+namespace ut
+{ }
+
+
+
+
+
+
+
+
