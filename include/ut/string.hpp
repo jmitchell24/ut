cc6e41bf78321cd195485fdb0bb5462c76b79e2a
James Mitchell <mail@jpmitchell.ca>
2025-06-21 09:14:04 -0400

    Jun 21 -- removed view_split.hpp

--- include/ut/string.hpp
+++ include/ut/string.hpp
@@ -4,7 +4,6 @@
 #include "ut/string/metric.hpp"
 #include "ut/string/builder.hpp"
 #include "ut/string/view.hpp"
-#include "ut/string/view_split.hpp"
 #include "ut/string/view_istream.hpp"
 
 namespace ut
