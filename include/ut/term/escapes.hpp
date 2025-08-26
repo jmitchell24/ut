8dd13d70d827d2f91802ed34b8d93812c8d80044
James Mitchell <mail@jpmitchell.ca>
2025-08-26 09:56:00 -0400

    Aug. 26 - Added oklch color support

--- include/ut/term/escapes.hpp
+++ include/ut/term/escapes.hpp
@@ -163,6 +163,7 @@ namespace ut::esc
     inline static TrueColorFg trueColorFg(color const& c) { return TrueColorFg{c}; }
     inline static TrueColorFg trueColorFg(color::hsv const& c) { return TrueColorFg{c.toColor()}; }
     inline static TrueColorFg trueColorFg(color::hsluv const& c) { return TrueColorFg{c.toColor()}; }
+    inline static TrueColorFg trueColorFg(color::oklch const& c) { return TrueColorFg{c.toColor()}; }
     inline static TrueColorFg trueColorFg(color::normal const& c) { return TrueColorFg{c.toColor()}; }
     inline static std::ostream& operator<< (std::ostream& os, TrueColorFg const& tc)
     { return os << tc.c.toFgEscCode(); }
@@ -171,6 +172,7 @@ namespace ut::esc
     inline static TrueColorBg trueColorBg(color const& c) { return TrueColorBg{c}; }
     inline static TrueColorBg trueColorBg(color::hsv const& c) { return TrueColorBg{c.toColor()}; }
     inline static TrueColorBg trueColorBg(color::hsluv const& c) { return TrueColorBg{c.toColor()}; }
+    inline static TrueColorBg trueColorBg(color::oklch const& c) { return TrueColorBg{c.toColor()}; }
     inline static TrueColorBg trueColorBg(color::normal const& c) { return TrueColorBg{c.toColor()}; }
     inline static std::ostream& operator<< (std::ostream& os, TrueColorBg const& tc)
     { return os << tc.c.toFgEscCode(); }
@@ -179,6 +181,7 @@ namespace ut::esc
     inline static TrueColor trueColor(color const& fg, color const& bg) { return TrueColor{fg, bg}; }
     inline static TrueColor trueColor(color::hsv const& fg, color::hsv const& bg) { return TrueColor{fg.toColor(), bg.toColor()}; }
     inline static TrueColor trueColor(color::hsluv const& fg, color::hsluv const& bg) { return TrueColor{fg.toColor(), bg.toColor()}; }
+    inline static TrueColor trueColor(color::oklch const& fg, color::oklch const& bg) { return TrueColor{fg.toColor(), bg.toColor()}; }
     inline static TrueColor trueColor(color::normal const& fg, color::normal const& bg) { return TrueColor{fg.toColor(), bg.toColor()}; }
     inline static std::ostream& operator<< (std::ostream& os, TrueColor const& tc)
     { return os << tc.fg.toFgEscCode() << tc.bg.toBgEscCode(); }
