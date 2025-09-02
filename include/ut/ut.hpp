c67e82775d6046b5d8637db6ed07016620b88fea
James Mitchell <mail@jpmitchell.ca>
2025-09-02 09:48:34 -0400

    Sep. 2 - working on binary_types.hpp

---
+++ include/ut/ut.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ut/options.hpp>
+#include <ut/algo.hpp>
+#include <ut/math.hpp>
+#include <ut/container.hpp>
+#include <ut/string.hpp>
+#include <ut/func.hpp>
+#include <ut/bit.hpp>
+#include <ut/time.hpp>
+#include <ut/color.hpp>
+#include <ut/random.hpp>
+#include <ut/check.hpp>
+
+#define UT_NAME         "ut"
+#define UT_VERSION      "25.9.2"
+
+#define UT_CLOWN_MODE
+
+namespace ut
+{ }
