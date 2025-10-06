e94b351dce25b33b34ccc60a6bd6e2c4b2ab3a17
James Mitchell <mail@jpmitchell.ca>
2025-10-06 10:42:49 -0400

    Oct. 6 - added Housekeeping Mode to CMakeLists.txt

--- include/ut/ut.hpp
+++ include/ut/ut.hpp
@@ -1,22 +1,21 @@
 #pragma once
 
-
 #include <ut/algo.hpp>
-#include <ut/math.hpp>
-#include <ut/container.hpp>
-#include <ut/string.hpp>
-#include <ut/func.hpp>
 #include <ut/bit.hpp>
 #include <ut/bit_exp.hpp>
-#include <ut/time.hpp>
-#include <ut/color.hpp>
-#include <ut/random.hpp>
 #include <ut/check.hpp>
-#include <ut/test.hpp>
-#include <ut/term.hpp>
-#include <ut/res.hpp>
-#include <ut/log.hpp>
+#include <ut/color.hpp>
+#include <ut/container.hpp>
+#include <ut/func.hpp>
 #include <ut/int.hpp>
+#include <ut/log.hpp>
+#include <ut/math.hpp>
+#include <ut/random.hpp>
+#include <ut/res.hpp>
+#include <ut/string.hpp>
+#include <ut/term.hpp>
+#include <ut/test.hpp>
+#include <ut/time.hpp>
 
 #define UT_NAME         "ut"
 #define UT_VERSION      "25.10.6"
