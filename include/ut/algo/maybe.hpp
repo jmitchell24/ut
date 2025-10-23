8aaf1a8d5a45682e552fa38155980da64c7c5f9f
James Mitchell <mail@jpmitchell.ca>
2025-10-23 12:18:37 -0400

    Oct. 23 - updated maybe.hpp

--- include/ut/algo/maybe.hpp
+++ include/ut/algo/maybe.hpp
@@ -9,8 +9,7 @@
 
 namespace ut
 {
-    // Iterator wrapper for optional
-    template<typename T> class optional_iterator
+    template<typename T> class maybe_iterator
     {
         T* ptr;
 
@@ -21,20 +20,20 @@ namespace ut
         using pointer           = T*;
         using reference         = T&;
 
-        constexpr optional_iterator(T* p = nullptr) : ptr(p) {}
+        constexpr maybe_iterator(T* p = nullptr) : ptr(p) {}
 
         constexpr reference operator*() const { return *ptr; }
         constexpr pointer operator->() const { return ptr; }
 
-        constexpr optional_iterator& operator++() { ptr = nullptr; return *this; }
-        constexpr optional_iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+        constexpr maybe_iterator& operator++() { ptr = nullptr; return *this; }
+        constexpr maybe_iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
 
-        constexpr bool operator==(optional_iterator const& other) const { return ptr == other.ptr; }
-        constexpr bool operator!=(optional_iterator const& other) const { return ptr != other.ptr; }
+        constexpr bool operator==(maybe_iterator const& other) const { return ptr == other.ptr; }
+        constexpr bool operator!=(maybe_iterator const& other) const { return ptr != other.ptr; }
     };
 
     // Const iterator wrapper
-    template<typename T> class optional_const_iterator
+    template<typename T> class maybe_const_iterator
     {
         T const* ptr;
 
@@ -45,16 +44,16 @@ namespace ut
         using pointer           = T const*;
         using reference         = T const&;
 
-        constexpr optional_const_iterator(T const* p = nullptr) : ptr(p) {}
+        constexpr maybe_const_iterator(T const* p = nullptr) : ptr(p) {}
 
         constexpr reference operator*() const { return *ptr; }
         constexpr pointer operator->() const { return ptr; }
 
-        constexpr optional_const_iterator& operator++() { ptr = nullptr; return *this; }
-        constexpr optional_const_iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+        constexpr maybe_const_iterator& operator++() { ptr = nullptr; return *this; }
+        constexpr maybe_const_iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
 
-        constexpr bool operator==(const optional_const_iterator& other) const { return ptr == other.ptr; }
-        constexpr bool operator!=(const optional_const_iterator& other) const { return ptr != other.ptr; }
+        constexpr bool operator==(const maybe_const_iterator& other) const { return ptr == other.ptr; }
+        constexpr bool operator!=(const maybe_const_iterator& other) const { return ptr != other.ptr; }
     };
 
     template <typename T>
@@ -62,11 +61,11 @@ namespace ut
     {
         T* ptr=nullptr;
 
-        auto begin(){ return optional_iterator<T>(ptr); }
-        auto end() { return optional_iterator<T>(nullptr); }
+        auto begin(){ return maybe_iterator<T>(ptr); }
+        auto end() { return maybe_iterator<T>(nullptr); }
 
-        auto begin() const{ return optional_iterator<T>(ptr); }
-        auto end() const { return optional_iterator<T>(nullptr); }
+        auto begin() const{ return maybe_const_iterator<T>(ptr); }
+        auto end() const { return maybe_const_iterator<T>(nullptr); }
     };
 }
 
@@ -79,19 +78,19 @@ namespace std
     // Non-member begin/end functions for std::optional
     template<typename T>
     constexpr auto begin(std::optional<T>& opt)
-    { return ut::optional_iterator<T>(opt ? &*opt : nullptr); }
+    { return ut::maybe_iterator<T>(opt ? &*opt : nullptr); }
 
     template<typename T>
     constexpr auto end(std::optional<T>& opt)
-    { return ut::optional_iterator<T>(nullptr); }
+    { return ut::maybe_iterator<T>(nullptr); }
 
     template<typename T>
     constexpr auto begin(const std::optional<T>& opt)
-    { return ut::optional_const_iterator<T>(opt ? &*opt : nullptr); }
+    { return ut::maybe_const_iterator<T>(opt ? &*opt : nullptr); }
 
     template<typename T>
     constexpr auto end(const std::optional<T>& opt)
-    { return ut::optional_const_iterator<T>(nullptr); }
+    { return ut::maybe_const_iterator<T>(nullptr); }
 }
 
 #endif // __cplusplus < 202600L && !defined(__cpp_lib_optional_range_support)
