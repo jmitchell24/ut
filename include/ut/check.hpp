c3ee0421fbcbc73992657b5105752a9d83f80b8f
James Mitchell <mail@jpmitchell.ca>
2026-01-05 09:21:30 -0500

    [ 2026-01-05 ] check.hpp prefix guard

--- include/ut/check.hpp
+++ include/ut/check.hpp
@@ -12,36 +12,50 @@
 
 
 #ifdef NDEBUG
-#define check_msg(...)          (void)0;
-#define constexpr_check(x_)     (void)0;
-#define nopath(...)             (void)0;
-#define nopath_case(x_)         (void)0;
+#define ut_check_msg(...)          (void)0;
+#define ut_constexpr_check(x_)     (void)0;
+#define ut_nopath(...)             (void)0;
+#define ut_nopath_case(x_)         (void)0;
 #else
-#define check_msg(x_, ...)  do { ::ut::internal::check_expression(x_, __LINE__, #x_, __func__, __FILE__, __VA_ARGS__); }while(0)
-#define nopath(...)         do { ::ut::internal::check_impossible(__LINE__, __func__, __FILE__, __VA_ARGS__); }while(0)
-#define nopath_case(x_)     do { static_assert(std::is_enum_v<x_>); ::ut::internal::check_impossible(__LINE__, __func__, __FILE__, "BAD ENUM CASE: %s", #x_); }while(0)
+#define ut_check_msg(x_, ...)  do { ::ut::internal::check_expression(x_, __LINE__, #x_, __func__, __FILE__, __VA_ARGS__); }while(0)
+#define ut_nopath_msg(...)     do { ::ut::internal::check_impossible(__LINE__, __func__, __FILE__, __VA_ARGS__); }while(0)
+#define ut_nopath_case(x_)     do { static_assert(std::is_enum_v<x_>); ::ut::internal::check_impossible(__LINE__, __func__, __FILE__, "BAD ENUM CASE: %s", #x_); }while(0)
 #endif
 
 // constexpr-compatible check: no-op at compile time, full check at runtime
 #if __cplusplus >= 202002L // C++20 constexpr support
 #include <type_traits>
-#define constexpr_check(x_) \
+#define ut_constexpr_check(x_) \
     do { if constexpr (!std::is_constant_evaluated()) \
-        { check_msg(x_, "SILENT CHECK"); } \
+        { ut_check_msg(x_, "SILENT CHECK"); } \
         else \
         { if ( (x_) == false ) throw std::runtime_error("SILENT CHECK"); } \
     } while(0)
 #else
-#define constexpr_check(x_)     check_msg(x_, "SILENT CHECK")
+#define ut_constexpr_check(x_)     ut_check_msg(x_, "SILENT CHECK")
 #endif
 
 #undef HAS_IS_CONSTANT_EVALUATED
 
-#define check(x_)           check_msg(x_, "SILENT CHECK")
-#define check_null(x_)      check_msg((x_) != nullptr, "NULL POINTER")
+#define ut_check(x_)           ut_check_msg(x_, "SILENT CHECK")
+#define ut_check_null(x_)      ut_check_msg((x_) != nullptr, "NULL POINTER")
 
-#define nopath_impl         nopath("UNIMPLEMENTED")
-#define nopath_break        nopath("BREAKPOINT")
+#define ut_nopath_impl         ut_nopath_msg("UNIMPLEMENTED")
+#define ut_nopath_break        ut_nopath_msg("BREAKPOINT")
+#define ut_nopath              ut_nopath_msg("IMPOSSIBLE");
+
+#ifndef UT_CHECK_PREFIXED_ONLY
+#define check_msg          ut_check_msg
+#define constexpr_check    ut_constexpr_check
+#define nopath             ut_nopath
+#define nopath_case        ut_nopath_case
+#define check              ut_check
+#define check_null         ut_check_null
+#define nopath_msg         ut_nopath_msg
+#define nopath_impl        ut_nopath_impl
+#define nopath_break       ut_nopath_break
+#define nopath             ut_nopath
+#endif
 
 namespace ut::internal
 {
